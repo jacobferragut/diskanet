e26124ca69393e3ee74c3d3f46971e6dc56b28ea
Erik Ferragut <erikferragut@gmail.com>
2020-06-10 09:39:40 -0400

    removed password from users, changed to get

--- server/prepare_orm2.py
+++ server/prepare_orm2.py
@@ -1,15 +1,16 @@
 import os
 import csv
+from datetime import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import click
+from flask import Flask
 
 from .diskanet_orm import base_app, User, Site
 #from .auth_orm import base as base_auth
 from .util import get_config
 
-import click
-from flask import Flask
-from datetime import datetime
 app = Flask(__name__)
 
 @app.cli.command('init-db')
@@ -24,22 +25,21 @@ def init_db():
         data = []
         R = csv.reader(open('server/users.csv', encoding='utf8'))
         for row in R:
-            if len(row[4]) <= 500:
-                data.append( User(
-                    user_id  = int(row[0]),
-                    name     = row[1],
-                    password = row[2],
-                    pass_salt= row[3],
-                    email    = row[4],
-                    email_confirmation = True,
-                    creation_date = datetime.now()
-                ))
+            data.append( User(
+                user_id  = int(row[0]),
+                name     = row[1],
+                # password = row[2],
+                # pass_salt= row[3],
+                email    = row[4],
+                email_confirmation = True,
+                creation_date = datetime.now()
+            ))
 
         print('Adding', len(data), 'rows to Usage table')
         session.add_all(data)
         session.commit()
-
-    if session.query(Site).count() <= 100:
+        
+    if session.query(Site).count() == 0:
         siteData = []
         ROWS = csv.reader(open('server/sites.csv', encoding='utf8'))
         for row in ROWS:
