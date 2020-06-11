4584904bea055c390c40b8aa5990d64805a796c7
nFerragut88 <nathanferragut@gmail.com>
2020-06-11 00:01:20 -0400

    finished get sites and post site etc

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,8 +1,10 @@
 from flask import Flask, g
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import delete
+
 from .diskanet_orm import User as duser
-from .diskanet_orm import Site
+from .diskanet_orm import Site as sites
 from flask_restx import Resource, Api
 import os
 from .util import get_config
@@ -31,36 +33,95 @@ class User(Resource):
         
     def put(self, user_id):
         '''update user's information'''
-        
         #gets user changes
         d = api.payload
+        if d is None: return {'msg': 'No change submitted'}
         
         #grabs user from database
-        user = g.db.query(duser).filter(duser.user_id == user_id)
-        
+        user = g.db.query(duser).get(user_id)
+        if user is None:
+            return {'msg': 'No user found'}
+            
+        #users can only change:
+        #name, email
+        allowed = ['name','email']
+        numchanged = 0
         #for each change for user
         for k,v in d.items():
-            setattr(user, k, v)
-            
+            if k in allowed:
+                setattr(user, k, v)
+                numchanged+=1
+        ##
+        if 'email' in d:
+            user.email_confirmation = False
         #commit the changes
+        if numchanged == 0:
+            return {'msg': 'you cant change those fields'}
         g.db.commit()
         
         #returns changes
-        return g.db.query(duser).get(user_id)
+        return str({ff: getattr(user, ff) for ff in d })
         
         
     def delete(self, user_id):
         '''delete user's account'''
-        return ""
+        user = g.db.query(duser).get(user_id)
+        if user is None: return {'msg': 'No user found'}
+        g.db.delete(user)
+        g.db.commit()
+        
+        return "deleted user:"
 
-@api.route('/site/<user_id>')
+@api.route('/site/<int:user_id>')
 class Sites(Resource):
-    def post(self):
+    def post(self, user_id):
         '''site creation'''
-        return ''
+        #Use JWT authentication to verify user is logged in to post a site
+        
+        
+        #grabs fields to create site
+        d = api.payload
+        if d is None: return {'msg': 'No change submitted'}
+        #need 
+        #site_id, name, title, body, owner_id (needs owner in the future)
+        
+        ###########
+        site = sites()
+        ##############
+        
+        #are we making site_id UUID or not??? For now im assuming we arent    
+        
+        #first do required column entries...
+        #and do allowed columns
+        allowed = ['title_font','title_font_size','body_font','body_font_size','background_color','genre_music','genre_art','genre_film','genre_writing']
+        required = ['site_id','name','title','body','owner_id']
+        reqNum = 0
+        for k,v in d.items():
+            if k in required:
+                setattr(site, k, v)
+                reqNum += 1
+            elif k in allowed:
+                setattr(site, k, v)
+        if reqNum != len(required):
+            return {'msg': 'ERROR: more fields required'}
+        
+        #user id of who owns the site
+        site.owner = g.db.query(duser).get(user_id)#use JWT authentication to get user creating the site
+        #commit the changes
+        g.db.add(site)
+        g.db.commit()
+        
+        
+        return 'site created'#add function later to return dict
+        
+        
     def get(self, user_id):
         '''get user's sites'''
-        return ''
+        #executes raw sql to grab user's sites 
+        s = g.db.execute(f'select * from sites where owner_id={user_id}')
+        return {ss.name: {ss.title:ss.body} for ss in s}
+        
+        
 @api.route('/site/<user_id>/<site_id>')
 class Site(Resource):
     def get(self, user_id, site_id):
