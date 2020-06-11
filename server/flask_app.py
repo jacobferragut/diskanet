29a6c18d64fadd90a94961748fc4429611214871
Jacob Ferragut <bonk3rs333@gmail.com>
2020-06-11 17:10:00 -0400

    old flask app

--- server/flask_app.py
+++ server/flask_app.py
@@ -10,6 +10,12 @@ from datetime import datetime
 app = Flask(__name__)
 api = Api(app)
 
+import hashlib
+
+from sqlalchemy import Column, types
+from sqlalchemy.ext.declarative import declarative_base
+
+
 @api.route('/user')
 class Users(Resource):
     def post(self):
@@ -78,3 +84,43 @@ def close_db(exception):
     if hasattr(g, 'db'):
         g.db.close()
         _ = g.pop('db')
+
+
+
+base = declarative_base()
+
+class Auth(base):
+    '''
+        user name: max str length = 80 and primary key
+        pass_hash: md5 hash of user_name, salt, and 
+        pass_salt: arbitrary string to harden  
+    '''
+    __tablename__ = 'auth'
+    user_id = Column(types.String(length=38), primary_key=True)
+    user_name = Column(types.String(length=80))
+    pass_hash = Column(types.String(length=32))
+    pass_salt = Column(types.String(length=40), default='')
+
+    def _compute_hash(self, password):
+        '''
+            Hash user_name, pass_salt, and password
+        '''
+        return hashlib.md5((self.user_name + '|' + self.pass_salt + '|' +
+               password).encode()).hexdigest()
+
+    def _set_hash(self, password):
+       '''
+          given knowns: user_name and pass_salt (compute and store pass_hash)
+          Used for new users. Does not commit.
+       '''
+       self.pass_hash = self._compute_hash(password)
+    
+    def _check_password(self, password):
+        '''
+            check if password agrees with pass_hash and pass_salt
+        '''
+        return self.pass_hash == self._compute_hash(password)
+
+
+
+
