96c46a540a48a1cc51235495364714a19c8b07da
Erik Ferragut <erikferragut@gmail.com>
2020-06-12 22:28:11 -0400

    remove extra auth_orm from flask_app

--- server/flask_app.py
+++ server/flask_app.py
@@ -279,7 +279,7 @@ def init_db():
         config = get_config(os.environ['FLASK_ENV'], open('server/config.yaml'))
         db = create_engine(config['DB'])
         g.db = sessionmaker(db)()
-    #create auth_db
+    # create auth_db
     if not hasattr(g, 'auth_db'):
         config = get_config(os.environ['FLASK_ENV'], open('server/config.yaml'))
         auth_db = create_engine(config['AUTH_DB'])
@@ -288,50 +288,12 @@ def init_db():
 @app.teardown_request
 def close_db(exception):
     '''close db connection'''
+    # close db
     if hasattr(g, 'db'):
         g.db.close()
         _ = g.pop('db')
 
-
-
-base = declarative_base()
-
-class Auth(base):
-    '''
-        user name: max str length = 80 and primary key
-        pass_hash: md5 hash of user_name, salt, and 
-        pass_salt: arbitrary string to harden  
-    '''
-    __tablename__ = 'auth'
-    user_id = Column(types.String(length=38), primary_key=True)
-    user_name = Column(types.String(length=80))
-    pass_hash = Column(types.String(length=32))
-    pass_salt = Column(types.String(length=40), default='')
-
-    def _compute_hash(self, password):
-        '''
-            Hash user_name, pass_salt, and password
-        '''
-        return hashlib.md5((self.user_name + '|' + self.pass_salt + '|' +
-               password).encode()).hexdigest()
-
-    def _set_hash(self, password):
-       '''
-          given knowns: user_name and pass_salt (compute and store pass_hash)
-          Used for new users. Does not commit.
-       '''
-       self.pass_hash = self._compute_hash(password)
-    
-    def _check_password(self, password):
-        '''
-            check if password agrees with pass_hash and pass_salt
-        '''
-        return self.pass_hash == self._compute_hash(password)
-
-
-
-
-    #close authentication db
+    # close authentication db
     if hasattr(g, 'auth_db'):
         g.auth_db.close()
         _ = g.pop('auth_db')
