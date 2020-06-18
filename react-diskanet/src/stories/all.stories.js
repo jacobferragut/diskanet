7d01acbe9eb7b5568c82d45d4f4b4a42e3bc72f2
nFerragut88 <nathanferragut@gmail.com>
2020-06-18 16:57:09 -0400

    seperated discover page

--- react-diskanet/src/stories/all.stories.js
+++ react-diskanet/src/stories/all.stories.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Banner, BoxPanel, RegisterScreen } from '../App.js';
+import { DiscoverScreen, FilterButton, FilterPanel } from '../Discover.js';
 
-
-storiesOf('banner', module)
-	.add('test', () => <BoxPanel>Test</BoxPanel>)
-	.add('test2', () => <Banner/>);
+storiesOf('MVP', module)
+	.add('BoxPanel', () => <BoxPanel>Test</BoxPanel>)
+	.add('Banner', () => <Banner/>)
+	.add('Filter', () => <DiscoverScreen></DiscoverScreen>)
