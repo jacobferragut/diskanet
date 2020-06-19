b34a94dca246015854bc2984da3de9c8ee3417d1
nFerragut88 <nathanferragut@gmail.com>
2020-06-19 12:11:53 -0400

    connecting discover to backend

--- react-diskanet/src/App.js
+++ react-diskanet/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Component } from 'react';
 import styled, { css } from 'styled-components'
+import axios from 'axios';
 
 import './App.css';
 import {DiscoverScreen} from './Discover.js';
 
+const APIURL = 'http://localhost:5000/';
+	submitSearch(genre_music, genre_art, genre_film, genre_writing) {
+		submission = Object()
+		if (genre_music 
+		axios.post(APIURL + 'api/discover', 
+			{ genre_music, genre_art, genre_film, genre_writing })
+			.then( response => {
+				console.log(response);
+			});
+	};
 
 const BoxPanel = styled.div`
 	display: inline-block;
