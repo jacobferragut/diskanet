a4025fa3e5ac2feb5c22d144eff86a07ced011ba
Jacob Ferragut <bonk3rs333@gmail.com>
2020-06-23 11:43:56 -0400

    basic login fields online

--- react-diskanet/src/App.js
+++ react-diskanet/src/App.js
@@ -50,27 +50,48 @@ class Banner extends Component {
 class RegisterScreen extends Component {
     constructor(props){
         super(props)
-        this.state = {username : '', password:'', passwordConfirmation:'', email:''}
-
+        this.state = {username : '', password:'', showPassword:'', email:''};
+        this.textType = "password"
 
         //this.logIt = this.logIt.bind(this)
         this.updateUsername = this.updateUsername.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
     }
     updateUsername(event) {
         this.setState({username: event.target.value});
+    }
+    updatePassword(event) {
+        this.setState({password: event.target.value});
+
+            
+    }
+    updateEmail(event){
+        this.setState({email: event.target.value});
+    }
+    updateShowPassword(event){
+        if (document.getElementsByName("password")[0].type=="password"){    
+            document.getElementsByName("password")[0].type="text"
+        }
+        else if(document.getElementsByName("password")[0].type=="text"){    
+            document.getElementsByName("password")[0].type="password"
+        }        
     }
     render(){
         return(
         <div>
             <form>
             <p>register</p>
-            email<input type="text" name="email"/>
+            email<input type="text" name="email" value={this.state.email}
+                onChange={this.updateEmail}/>
         	username<input type="text" value={this.state.username}
 	            onChange={this.updateUsername}/>
 	        <br/>
-        	password<input type="password" value={this.state.password} name="password"/>
+        	password<input type="password" value={this.state.password} name="password"
+        	    onChange={this.updatePassword}/>
 	        <br/>
-        	show password<input type="checkbox" value="" name="passwordConfirmation"/>
+        	show password<input type="checkbox" value="" name="ShowPassword"
+        	    onChange={this.updateShowPassword}/>
 	        <br/>
 	        <button onClick={ this.goRegister} 
 	            name='registerButton'>register</button>
