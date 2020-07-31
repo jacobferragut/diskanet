798af97cffb4b49602f9532ec2efcc835b853715
Jacob Ferragut <bonk3rs333@gmail.com>
2020-07-31 09:50:55 -0400

    new build

---
+++ react-diskanet/build/precache-manifest.25d80ccf09bede5d3e5e724ad959d629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9aa2d5f3dd824cf1bd3d39609f5aea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54103c199bfbdeca087f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2f4711d53e9daa8375e2",
+    "url": "/static/css/main.53a15f1a.chunk.css"
+  },
+  {
+    "revision": "54103c199bfbdeca087f",
+    "url": "/static/js/2.83b4c7d2.chunk.js"
+  },
+  {
+    "revision": "217f038bbfd95eda289ef5578706dcdb",
+    "url": "/static/js/2.83b4c7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4711d53e9daa8375e2",
+    "url": "/static/js/main.5b2575a2.chunk.js"
+  },
+  {
+    "revision": "cb371a2ff2ad2d7b1f1f",
+    "url": "/static/js/runtime-main.27f22f25.js"
+  }
+]);
