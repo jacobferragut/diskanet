aab55dc4f677c606af240af003077f2ab8247843
Jacob Ferragut <bonk3rs333@gmail.com>
2020-07-31 09:36:29 -0400

    added build and fixed apiurl

---
+++ react-diskanet/build/precache-manifest.2ba159ad7ef3e4d05cc27ef281bd206b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3a17981dda1907cfda8b8726ead0e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54103c199bfbdeca087f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2dbda95d2afac070ed25",
+    "url": "/static/css/main.53a15f1a.chunk.css"
+  },
+  {
+    "revision": "54103c199bfbdeca087f",
+    "url": "/static/js/2.83b4c7d2.chunk.js"
+  },
+  {
+    "revision": "217f038bbfd95eda289ef5578706dcdb",
+    "url": "/static/js/2.83b4c7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbda95d2afac070ed25",
+    "url": "/static/js/main.0815d5df.chunk.js"
+  },
+  {
+    "revision": "cb371a2ff2ad2d7b1f1f",
+    "url": "/static/js/runtime-main.27f22f25.js"
+  }
+]);
